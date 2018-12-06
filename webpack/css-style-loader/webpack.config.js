c8b15469a69898389b9bc014c9f1b83e48a17989
Jorge Rangel <georgerangelcode@gmail.com>
2018-12-06 09:01:21 -0500

    Loaders

---
+++ webpack/css-style-loader/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+	entry: path.resolve(__dirname, 'index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			// Aqui van los loaders
+			{
+				// test: Que tipo de archivo quiero reconocer
+				// use: Que loader se va a encargar del archivo
+				test: /\.css$/,
+				use: ['style-loader',
+					  'css-loader'
+					 ],
+			}
+		]
+	}
+}
